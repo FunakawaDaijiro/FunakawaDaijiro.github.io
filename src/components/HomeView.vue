9ccf132f441906daca96414aed8e06d985b63adb
Funakawa Daijiro <you@example.com>
2023-09-22 15:03:24 +0900

    init

---
+++ src/components/HomeView.vue
@@ -0,0 +1,28 @@
+<template>
+	<div>
+		<div class="text-center m-5">
+			<img alt="logo" src="../assets/icon.jpg" class="m-auto w-80 rounded-3xl" loading=”lazy”>	
+			<p class="text-2xl font-momo">船川 大二郎</p>
+			<p class="text-lg font-momo text-slate-500">大学生</p>
+		</div>
+
+		<div class="w-60 mx-auto my-10 flex flex-col gap-4">
+			<button class="sns-btn" type="button" onclick="location.href='https://github.com/FunakawaDaijiro/'">
+				<img src="../assets/github.svg" alt="GitHub icon" class="w-10" loading=”lazy”>
+				<span class="mx-auto">GitHub</span>
+			</button>
+		</div>
+		
+		<div>
+			<p class="headline">About me</p>
+			<p>
+				情報系。<br>
+				現在、動画像処理による魚の異常行動検知に関する研究を行っている。
+				<span class="md:inline block">
+					専門分野はCV
+				</span>
+				<br>
+			</p>
+		</div>
+	</div>
+</template>
